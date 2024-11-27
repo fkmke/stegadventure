202bb359c21890f4453be1049be2206615c2ecfb
Femke Weijsenfeld <femkeweijsenfeld2003@gmail.com>
2024-11-27 18:04:41 +0100

    First layout setup

--- resources/js/components/TextTemplate.vue
+++ resources/js/components/TextTemplate.vue
@@ -1,5 +1,11 @@
 <template>
     <div id="background">
+        <div class="text-object title">
+            <h1>Stegoknow</h1>
+        </div>
+        <div class="text-object">
+            <p>Hello, welcome!</p>
+        </div>
     </div>
 </template>
 
@@ -9,6 +15,25 @@
     background-position: top;
     background-size: cover;
     background-repeat: no-repeat;
-    height: 100vh;
+    background-attachment: fixed;
+    height: 100%;
+    min-height: 100vh;
+    padding: var(--standard-distance);
+}
+
+.text-object {
+    padding: var(--standard-padding);
+    background-color: var(--primary-container);
+    color: var(--on-primary-container);
+    width: var(--page-width);
+    margin: auto;
+    border-radius: var(--rounded-big);
+    margin-bottom: var(--standard-padding);
+    border: var(--border-width) solid var(--secondary);
+}
+
+.title {
+    background-color: var(--secondary-container);
+    color: var(--on-secondary-container);
 }
 </style>
