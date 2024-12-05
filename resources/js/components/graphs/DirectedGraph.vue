9a76ef258eacb32ad73cc90cf11732d8d96adb1a
Femke Weijsenfeld <femkeweijsenfeld2003@gmail.com>
2024-12-05 15:28:53 +0100

    Phase 1 of chapter 5

--- resources/js/components/graphs/DirectedGraph.vue
+++ resources/js/components/graphs/DirectedGraph.vue
@@ -64,7 +64,7 @@ function createGraph() {
     const simulation = d3
         .forceSimulation(props.nodes)
         .force("link", d3.forceLink(props.links).id((d) => d.id).distance(100))
-        .force("charge", d3.forceManyBody().strength(-150))
+        .force("charge", d3.forceManyBody().strength(-200))
         .force("center", d3.forceCenter(width / 2 / 0.2, height / 2 / 0.2));
 
     // Add links
