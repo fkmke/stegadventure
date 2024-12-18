4f627f8ae1ece8314341fbf966034860c9d2539b
Femke Weijsenfeld <femkeweijsenfeld2003@gmail.com>
2024-12-18 15:20:12 +0100

    Complete written text

--- resources/js/views/SteganographyText.vue
+++ resources/js/views/SteganographyText.vue
@@ -5,6 +5,7 @@ import axios from 'axios';
 // Images
 import encryptionScheme from '../../img/text/Asymmetric_encryption_scheme.png';
 import caesarCipher from '../../img/text/Caesar_Shift_Cipher_Wheel.png';
+import uvInk from '../../img/text/Written_text_with_UV_ink.jpg';
 
 const props = defineProps({
     experimentState: Number,
@@ -40,14 +41,15 @@ function next() {
             systems must be protected.
         </p>
         <p>
-            This reading will focus on safe and secret transmissions. We will start with a very known concept:
+            This reading will focus on safe and secret communication. We will start with a very known concept:
             cryptography. Then we will focus in the main topic: steganography.
         </p>
 
         <h3>Cryptography</h3>
         <p>
             In cryptography, a plaintext will be encrypted. The plaintext is the readable message that needs to be send.
-            When the plaintext is encrypted with a encryption key, it should be unreadable for a possible interceptor of
+            When the plaintext is encrypted with an encryption key, it should be unreadable for a possible interceptor
+            of
             the message. The receiver can then decrypt the message with the decryption key.
         </p>
 
@@ -56,8 +58,9 @@ function next() {
             <p><b>Figure 1:</b> Caesar Cipher Wheel for shifting letters.</p>
         </div>
         <p>
-            A famous basic cryptography algorithm is the Caesar Cipher, in which each alphabet letter is shifted by. For
-            example, you can replace all 'A' by 'B'. 'BAG' will then become 'CBH'. The encryption key is the number of
+            A famous basic cryptography algorithm is the Caesar Cipher, in which each alphabet letter is shifted by a
+            certain amount. For example, you can replace all 'A' by 'B'. 'BAG' will then become 'CBH'. The encryption
+            key is the number of
             shifted letters, which was 1 in the previous sentence. The decryption key is -1, since all letters should be
             shifted one back in the alphabet. The Caesar Chipher Wheel for key 19 is shown in
             <a href="#figure-1">Figure 1</a>.
@@ -70,7 +73,8 @@ function next() {
             everyone, and makes sure nobody knows the private key. People who want to send her a message will encrypt
             their message with the public key. After encryption, even the sender cannot decrypt the message anymore.
             When Alice receives the message, she can decrypt it with her private key. As long as the private key is not
-            known or cannot be guessed, the message's content is safe.
+            known or cannot be guessed, the message's content is safe. This process is illustrated in
+            <a href="#figure-2">Figure 2</a>.
         </p>
         <div id="figure-2" class="image-wrapper">
             <img :src="encryptionScheme" />
@@ -81,17 +85,24 @@ function next() {
         <p>
             Steganography is a very different principal of secret communication. Instead of making the send text
             unreadable, you hide the existence of the secret message. An example of this is writing a letter which seems
-            no harm, but instead you have written the real message within the lines with invisible ink.
+            no harm, but instead you have written the real message within the lines with invisible ink
+            (<a href="#figure-3">Figure 3</a>).
         </p>
+        <div id="figure-3" class="image-wrapper">
+            <img :src="uvInk" />
+            <p><b>Figure 3:</b> Something secret can be written with UV ink on an unharmful looking document.</p>
+        </div>
         <p>
-            One of the first records of steganography is from 499BC. The ruler of Miletus shaved the head of one of his
+            One of the first records of steganography is from 499BC. The ruler of Miletus, Histieaus, shaved the head of
+            one of his
             most trusted slaves and tattooed a message. Then he waited untill the hair was grown back and send him to
             Aristagoras, who shaved the head and read the intruction to revolt against the Persians. The hidden message
             was tattooed and the cover was the slave.
         </p>
         <p>
-            In the digital world, messages can be hidden within digital files, like audio fragments, PDF files, images,
-            and many more digital media. These files are represented by bits, and by adjusting or adding bits, secret
+            In the digital world, messages can be hidden within digital files as cover, like audio fragments, PDF files,
+            images, and many more digital media. These files are represented by bits, and by adjusting or adding bits,
+            secret
             messages can be embedded into these files.
         </p>
 
@@ -104,20 +115,20 @@ function next() {
         <p>
             Cryptography is therefore used to make the message's content secret, while steganography is used to hide the
             existence of a secret message. They are not mutually exclusive, because you can hide an encrypted message
-            within a digital file.
+            as well.
         </p>
 
         <h2>Relevance</h2>
         <p>
             Steganography can be used as a communication method. For example, it is said that Al-Qaeda used it in
             preparation for the 9/11 suicide attacks. Unharmful looking images were posted online. Those contained
-            secret messages with instructions for the attack.
+            secret messages with communications for the attack.
         </p>
         <p>
             Research also shows that alarming number of images nowadays seem to contain steganography on eBay. This is
             concluded by using StegAnalysis tools to check with either statistics or AI whether a hidden message may be
             embedded inside the image. Still, since steganography is the art of hiding, it is unclear what message was
-            contained and if steganography was really used. People can just post images anonymously and others can
+            contained and if steganography was really used. But people can just post images anonymously and others can
             download them anonymously to retrieve the message.
         </p>
         <p>
@@ -142,12 +153,12 @@ function next() {
             (in decimal: 255, 101, 194).
         </p>
         <p>
-            When flip from each colour value the least significant bit, thus we get <span
-                class="color-2">1111111<b>0</b> 0110010<b>0</b> 1100001<b>1</b></span>, the change
+            When we flip the least significant bit of each colour value, we get <span class="color-2">1111111<b>0</b>
+                0110010<b>0</b> 1100001<b>1</b></span>. The change
             in colour is present, but barely visible with the human eye in an image with lots of noise.
         </p>
         <p>
-            Since changing this last bit does not influence the picture very much we can use those bits to hide a
+            Since changing this last bit does not influence the image very much we can use those bits to hide a
             message! Let's say our protocol is that each letter of the alphabet is the number in the alphabet (e.g. 'a'
             is 1, and 'y' is 25). Then we can represent 'y' as 0110001 in binary. We can then change each of the least
             significant bits of the image to put the letter 'y' hidden inside the image, like so:<br />
@@ -162,20 +173,40 @@ function next() {
         </p>
 
         <h3>Stego key</h3>
-        <p>Why stego key?</p>
+        <p>
+            If all least significant bits are used for the message, someone may then easily extract the message if they
+            suspect its existence. Therefore, a stego key can be introduced. A stego key will randomize which bits are
+            used for the message. Then only people who have access to this stego key can extract the secret message.
+            Randomization of which bits are used for steganography may also make it harder for StegAnalysis tools to see
+            whether the image contains steganography or not.
+        </p>
 
         <h2>StegWare</h2>
         <p>
-            Steganography Malware
+            Malware that makes use of steganography is called StegWare. The main idea of StegWare is to avoid
+            anti-malware systems by hiding the attack in another digital object (e.g. PDF, image, Excel sheet, etc.).
+            This way, anti-malware systems may not see the existence of such attack. While encryption hides the exact
+            attack, it does not hide its possible existence and will therefore usually be blocked by Intrusion Detection
+            Systems. Steganography hides the existence of the attack, making it hard to detect.
         </p>
         <p>
-            Steal credit card numbers
+            For example, an PDF with malware embedded within may be shared through email. If the receiver downloads
+            and opens the PDF, there is a possibility that the hidden malware will be extracted from the PDF and be
+            executed. Ransomware may block a computer this way. Therefore, it is advised to not download files of
+            unknown origin.
         </p>
         <p>
-            Why? Hard to detect.
+            This is not the only possibility. Hackers have been able to put Javascript code within images on compromised
+            webshops. When people fill in their credit card details, they are also send to the hackers. The webshop may
+            even not be aware of this happening. They should be up to date with the latest cyber security defense to
+            prevent this from happening.
         </p>
+
+        <h2>Summary</h2>
         <p>
-            Pseudocode
+            All in all, steganography is a method for hiding messages. For example, these messages can hide secret
+            communication, or they can hide an cyber attack. In an increasingly digital world, people should be aware of
+            the existence of steganography.
         </p>
 
         <div class="button">
@@ -196,8 +227,10 @@ img {
     text-align: center;
     flex-direction: column;
     justify-content: center;
-    margin-bottom: 10px;
+    margin-bottom: 15px;
+    margin-top: 15px;
     gap: 5px;
+    font-style: italic
 }
 
 .color-1,
