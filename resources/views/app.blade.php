c382d1e12f61df6ab0b09e61a3551a1c5ee2fd91
Femke Weijsenfeld <femkeweijsenfeld2003@gmail.com>
2025-01-06 10:24:01 +0100

    Added meta description

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <meta name="description"
+        content="StegAdventure, the game designed to give students an introduction
+        to the topic of steganography, while having fun. Perfect for students learning
+        about cybersecurity without preknowledge on steganography.">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>StegAdventure</title>
